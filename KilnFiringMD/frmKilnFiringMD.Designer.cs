876551129bb2074095f2b087b91c77b8d09b3b4c
Brian Muth <bmuth@telus.net>
2023-04-25 20:05:06 -0700

    Store pic in database

--- KilnFiringMD/frmKilnFiringMD.Designer.cs
+++ KilnFiringMD/frmKilnFiringMD.Designer.cs
@@ -31,14 +31,17 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.TempChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dgvFiring = new System.Windows.Forms.DataGridView();
             this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.created_at = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pbPic = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.TempChart)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFiring)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPic)).BeginInit();
             this.SuspendLayout();
             // 
             // TempChart
@@ -63,18 +66,28 @@
             // 
             // dgvFiring
             // 
+            this.dgvFiring.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dgvFiring.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvFiring.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.id,
             this.title,
             this.created_at,
             this.description});
-            this.dgvFiring.Location = new System.Drawing.Point(111, 32);
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvFiring.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvFiring.Location = new System.Drawing.Point(12, 37);
             this.dgvFiring.Name = "dgvFiring";
             this.dgvFiring.Size = new System.Drawing.Size(785, 159);
             this.dgvFiring.TabIndex = 1;
             this.dgvFiring.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFiring_CellValueChanged);
             this.dgvFiring.ColumnWidthChanged += new System.Windows.Forms.DataGridViewColumnEventHandler(this.dgvFiring_ColumnWidthChanged);
+            this.dgvFiring.RowLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFiring_RowLeave);
             this.dgvFiring.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dgvFiring_RowStateChanged);
             this.dgvFiring.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dgvFiring_UserDeletingRow);
             // 
@@ -102,11 +115,23 @@
             this.description.HeaderText = "Description";
             this.description.Name = "description";
             // 
+            // pbPic
+            // 
+            this.pbPic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbPic.Location = new System.Drawing.Point(812, 37);
+            this.pbPic.Name = "pbPic";
+            this.pbPic.Size = new System.Drawing.Size(244, 158);
+            this.pbPic.TabIndex = 2;
+            this.pbPic.TabStop = false;
+            this.pbPic.DragEnter += new System.Windows.Forms.DragEventHandler(this.pbPic_DragEnter);
+            this.pbPic.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbPic_MouseClick);
+            // 
             // frmKilnFiringMD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1069, 635);
+            this.Controls.Add(this.pbPic);
             this.Controls.Add(this.dgvFiring);
             this.Controls.Add(this.TempChart);
             this.Name = "frmKilnFiringMD";
@@ -116,6 +141,7 @@
             this.Shown += new System.EventHandler(this.frmKilnFiringMD_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.TempChart)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFiring)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPic)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -128,6 +154,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn title;
         private System.Windows.Forms.DataGridViewTextBoxColumn created_at;
         private System.Windows.Forms.DataGridViewTextBoxColumn description;
+        private System.Windows.Forms.PictureBox pbPic;
     }
 }
 
--- KilnFiringMD/frmKilnFiringMD.cs
+++ KilnFiringMD/frmKilnFiringMD.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +23,8 @@ namespace KilnFiringMD
         private System.Collections.Specialized.StringCollection ColumnWidths;
         private bool m_bEditingEnabled = false;
         private string m_ConnectionString;
+        private bool m_DirtyPicture = false;
+        private int m_CurrentPic = -1;
 
         public frmKilnFiringMD ()
         {
@@ -123,13 +128,15 @@ namespace KilnFiringMD
         }
         private void frmKilnFiringMD_Shown (object sender, EventArgs e)
         {
+            pbPic.SizeMode = PictureBoxSizeMode.CenterImage;
+
             bSettingWidthsToBeSaved = true; // can save now
             dgvFiring.Columns[2].DefaultCellStyle.Format = "yyyy-MMM-dd HH:mm:ss";
 
             /*          Load data
              *          --------- */
 
-            var ds = ExecuteDataSet ("SELECT * from FiringRun");
+            var ds = ExecuteDataSet ("SELECT id,title,started_at,description from FiringRun");
 
             if (ds != null)
             {
@@ -201,6 +208,47 @@ namespace KilnFiringMD
             }
         }
 
+        private void DisplayPicture (int id)
+        {
+            string query = "SELECT pic FROM FiringRun WHERE id=" + id.ToString ();
+            using (var con = new MySqlConnection (m_ConnectionString))
+            {
+                con.Open ();
+
+                if (m_CurrentPic != id)
+                {
+                    using (MySqlCommand cmd = new MySqlCommand (query, con))
+                    {
+                        using (MySqlDataReader r = cmd.ExecuteReader ())
+                        {
+
+                            Byte[] image = null;
+                            if (r.Read ()) // advance to next (first) record
+                            {
+                                if (r["pic"] != System.DBNull.Value)
+                                {
+                                    image = (Byte[]) (r["pic"]);
+                                }
+                            }
+
+                            if (image != null)
+                            {
+                                MemoryStream ms = new MemoryStream ();
+                                ms.Write (image, 0, image.Length);
+                                pbPic.Image = System.Drawing.Image.FromStream (ms, false, true);
+
+                            }
+                            else
+                            {
+                                pbPic.Image = null;
+                            }
+                        }
+                    }
+                    m_CurrentPic = id;
+                }
+            }
+        }
+
         private void DisplayKilnData (DateTime start, string description, int id)
         {
             StringBuilder sql = new StringBuilder ("SELECT temp, time From DataPoint WHERE fid=");
@@ -235,7 +283,9 @@ namespace KilnFiringMD
         {
             if (e.StateChanged == DataGridViewElementStates.Selected)
             {
-                 DisplayKilnData ((DateTime) e.Row.Cells[2].Value, (string) e.Row.Cells[3].Value, (int) e.Row.Cells[0].Value);
+                DisplayKilnData ((DateTime) e.Row.Cells[2].Value, (string) e.Row.Cells[3].Value, (int) e.Row.Cells[0].Value);
+                DisplayPicture ((int) e.Row.Cells[0].Value);
+
             }
         }
 
@@ -274,23 +324,23 @@ namespace KilnFiringMD
 
         private void TempChart_MouseClick (object sender, MouseEventArgs e)
         {
-            var r = TempChart.HitTest (e.X, e.Y);
-            if (r.ChartElementType == ChartElementType.DataPoint)
-            {
+            //var r = TempChart.HitTest (e.X, e.Y);
+            //if (r.ChartElementType == ChartElementType.DataPoint)
+            //{
 
-                DataPoint p = (DataPoint) r.Object;
-                Debug.Print (string.Format ("{0} {1}", p.XValue, p.YValues[0]));
+            //    DataPoint p = (DataPoint) r.Object;
+            //    Debug.Print (string.Format ("{0} {1}", p.XValue, p.YValues[0]));
 
-                var pt = new PointF ((float) TempChart.Series[0].Points[r.PointIndex].XValue, (float) TempChart.Series[0].Points[r.PointIndex].YValues[0]);
-                CalloutAnnotation ca = new CalloutAnnotation ();
-                ca.AnchorDataPoint = TempChart.Series[0].Points[r.PointIndex];
-                ca.Text = pt.ToString ();
-                ca.CalloutStyle = CalloutStyle.SimpleLine;
-                ca.ForeColor = Color.Red;
-                ca.Font = new Font ("Tahoma", 12, FontStyle.Bold);
-                TempChart.Annotations[0] = ca;
-                TempChart.Invalidate ();
-            }
+            //    var pt = new PointF ((float) TempChart.Series[0].Points[r.PointIndex].XValue, (float) TempChart.Series[0].Points[r.PointIndex].YValues[0]);
+            //    CalloutAnnotation ca = new CalloutAnnotation ();
+            //    ca.AnchorDataPoint = TempChart.Series[0].Points[r.PointIndex];
+            //    ca.Text = pt.ToString ();
+            //    ca.CalloutStyle = CalloutStyle.SimpleLine;
+            //    ca.ForeColor = Color.Red;
+            //    ca.Font = new Font ("Tahoma", 12, FontStyle.Bold);
+            //    TempChart.Annotations[0] = ca;
+            //    TempChart.Invalidate ();
+            //}
         }
 
         private void TempChart_MouseMove (object sender, MouseEventArgs e)
@@ -320,12 +370,116 @@ namespace KilnFiringMD
         private void frmKilnFiringMD_Load (object sender, EventArgs e)
         {
             TempChart.Annotations.Add (new CalloutAnnotation());
+            pbPic.AllowDrop = true;
         }
 
         private void TempChart_MouseLeave (object sender, EventArgs e)
         {
             TempChart.Annotations[0] = new CalloutAnnotation ();
             TempChart.Invalidate ();
+        }
+
+        private void pbPic_DragEnter (object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent (DataFormats.Bitmap))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /***********************************************************************
+         * Resize the image to the specified width and height.
+         *
+         *
+         * <param name="image">The image to resize.</param>
+         * <param name="width">The width to resize to.</param>
+         * <param name="height">The height to resize to.</param>
+         * <returns>The resized image.</returns>
+         * 
+         * *********************************************************************/
+
+        public static Bitmap ResizeImage (Image image, int width, int height)
+        {
+            var destRect = new Rectangle (0, 0, width, height);
+            var destImage = new Bitmap (width, height);
+
+            destImage.SetResolution (image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage (destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes ())
+                {
+                    wrapMode.SetWrapMode (WrapMode.TileFlipXY);
+                    graphics.DrawImage (image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+        private void pbPic_MouseClick (object sender, MouseEventArgs e)
+        {
+            if (Clipboard.ContainsImage ())
+            {
+                Image image = Clipboard.GetImage ();
+                double x_shrink = (double) pbPic.ClientSize.Width / (double) image.Width;
+                double y_shrink = (double) pbPic.ClientSize.Height / (double) image.Height;
+                Bitmap b;
+                if (y_shrink < x_shrink) 
+                {
+                    b = ResizeImage (image, (int) (image.Width * y_shrink), (int) (image.Height * y_shrink));
+                }
+                else
+                {
+                    b = ResizeImage (image, (int) (image.Width * x_shrink), (int) (image.Height * x_shrink));
+
+                }
+                pbPic.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                pbPic.Image = b;
+                m_DirtyPicture = true;
+            }
+        }
+
+        private async void dgvFiring_RowLeave (object sender, DataGridViewCellEventArgs e)
+        {
+            if (m_DirtyPicture)
+            {
+                var dr = MessageBox.Show ("Do you want to save the picture?", "Confirm save picture", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                {
+                    using (MemoryStream ms = new MemoryStream ())
+                    {
+                        pbPic.Image.Save (ms, System.Drawing.Imaging.ImageFormat.Png);
+                        byte[] img = ms.ToArray ();
+
+                        using (var con = new MySqlConnection (m_ConnectionString))
+                        {
+                            con.Open ();
+
+                            var cmd = new MySqlCommand ("UPDATE FiringRun SET pic = @pic WHERE id = @id", con);
+
+                            cmd.Parameters.Add ("@id", MySqlDbType.Int32);
+                            cmd.Parameters.Add ("@pic", MySqlDbType.MediumBlob);
+
+                            cmd.Parameters["@id"].Value = dgvFiring.Rows[e.RowIndex].Cells[0].Value;
+                            cmd.Parameters["@pic"].Value = img;
+
+                            await cmd.ExecuteNonQueryAsync ();
+                        }
+                    }
+                }
+                m_DirtyPicture = false;
+            }
         }
     }
 }
